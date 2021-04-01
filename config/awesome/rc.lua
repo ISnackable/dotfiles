50cd9f43975023dae7eb0b97a27a23c64aa08be6
Tommy <52971804+ISnackable@users.noreply.github.com>
2021-04-02 05:28:57 +0800

    Clean up configs

---
+++ config/awesome/rc.lua
@@ -0,0 +1,465 @@
+--[[
+
+ ▄▄▄       █     █░▓█████   ██████  ▒█████   ███▄ ▄███▓▓█████
+▒████▄    ▓█░ █ ░█░▓█   ▀ ▒██    ▒ ▒██▒  ██▒▓██▒▀█▀ ██▒▓█   ▀
+▒██  ▀█▄  ▒█░ █ ░█ ▒███   ░ ▓██▄   ▒██░  ██▒▓██    ▓██░▒███
+░██▄▄▄▄██ ░█░ █ ░█ ▒▓█  ▄   ▒   ██▒▒██   ██░▒██    ▒██ ▒▓█  ▄
+ ▓█   ▓██▒░░██▒██▓ ░▒████▒▒██████▒▒░ ████▓▒░▒██▒   ░██▒░▒████▒
+ ▒▒   ▓▒█░░ ▓░▒ ▒  ░░ ▒░ ░▒ ▒▓▒ ▒ ░░ ▒░▒░▒░ ░ ▒░   ░  ░░░ ▒░ ░
+  ▒   ▒▒ ░  ▒ ░ ░   ░ ░  ░░ ░▒  ░ ░  ░ ▒ ▒░ ░  ░      ░ ░ ░  ░
+  ░   ▒     ░   ░     ░   ░  ░  ░  ░ ░ ░ ▒  ░      ░      ░
+
+---- >> The file that binds everything together.
+--]]
+
+modkey = "Mod4" -- MOVE to `keys.lua` later
+
+-- Theme selection variable
+local theme = "HorizonInspired"
+-- ===================================================================
+-- User variables and preferences
+user = {
+    -- >> Default applications <<
+    -- Check apps.lua for more
+    terminal = "alacritty",
+    browswer = "firefox",
+    file_manager = "thunar",
+    editor = "vim",
+
+    -- >> User profile <<
+    profile_picture = os.getenv("HOME").."/.config/awesome/profle.png",
+
+    -- Directories
+    dirs = {
+        downloads = "~/Downloads",
+        documents = "~/Documents",
+        music = "~/Music",
+        pictures = "~/Pictures",
+        videos = "~/Videos",
+        -- Make sure the directory exists so that your screenshots are not lost
+        screenshots = "~/Pictures/Screenshots",
+    },
+
+    -- >> Sidebar <<
+    sidebar = {
+        hide_on_mouse_leave = true,
+        show_on_mouse_screen_edge = true,
+    },
+
+    -- >> Battery <<
+    -- You will receive notifications when your battery reaches these levels.
+    battery_threshold_low = 20,
+    battery_threshold_critical = 5
+}
+-- ===================================================================
+
+-- Initialization
+-- ===================================================================
+-- Theme handling library
+local beautiful = require("beautiful")
+local xrdb = beautiful.xresources.get_current_theme()
+-- Make dpi function global
+dpi = beautiful.xresources.apply_dpi
+-- Make xresources colors global
+x = {
+    --           xrdb variable
+    background = xrdb.background,
+    foreground = xrdb.foreground,
+    color0     = xrdb.color0,
+    color1     = xrdb.color1,
+    color2     = xrdb.color2,
+    color3     = xrdb.color3,
+    color4     = xrdb.color4,
+    color5     = xrdb.color5,
+    color6     = xrdb.color6,
+    color7     = xrdb.color7,
+    color8     = xrdb.color8,
+    color9     = xrdb.color9,
+    color10    = xrdb.color10,
+    color11    = xrdb.color11,
+    color12    = xrdb.color12,
+    color13    = xrdb.color13,
+    color14    = xrdb.color14,
+    color15    = xrdb.color15,
+}
+
+-- Standard awesome library
+-- ===================================================================
+local gears = require("gears")
+local awful = require("awful")
+require("awful.autofocus")
+-- Widget and layout library
+local wibox = require("wibox") -- MOVE TO `noodle` directory later
+-- Notification library
+local naughty = require("naughty")
+local menubar = require("menubar") -- MOVE TO `notification` directory later
+
+-- Load theme, themes define colours, icons, font and wallpapers. 
+local theme_dir = os.getenv("HOME") .. "/.config/awesome/themes/" .. theme .. "/"
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+
+-- Error handling
+-- ===================================================================
+-- Check if awesome encountered an error during startup and fell back to another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.connect_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = tostring(err) })
+        in_error = false
+    end)
+end
+
+-- Features
+-- ===================================================================
+-- Keybinds and mousebinds
+local keys = require("keys")
+
+-- Menubar configuration
+menubar.utils.terminal = user.terminal -- Set the terminal for applications that require it
+-- }}}
+
+-- Keyboard map indicator and switcher
+mykeyboardlayout = awful.widget.keyboardlayout()
+
+-- {{{ Wibar
+-- Create a textclock widget
+mytextclock = wibox.widget.textclock()
+
+-- Create a wibox for each screen and add it
+local taglist_buttons = gears.table.join(
+                    awful.button({ }, 1, function(t) t:view_only() end),
+                    awful.button({ modkey }, 1, function(t)
+                                              if client.focus then
+                                                  client.focus:move_to_tag(t)
+                                              end
+                                          end),
+                    awful.button({ }, 3, awful.tag.viewtoggle),
+                    awful.button({ modkey }, 3, function(t)
+                                              if client.focus then
+                                                  client.focus:toggle_tag(t)
+                                              end
+                                          end),
+                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+                )
+
+local tasklist_buttons = gears.table.join(
+                     awful.button({ }, 1, function (c)
+                                              if c == client.focus then
+                                                  c.minimized = true
+                                              else
+                                                  c:emit_signal(
+                                                      "request::activate",
+                                                      "tasklist",
+                                                      {raise = true}
+                                                  )
+                                              end
+                                          end),
+                     awful.button({ }, 3, function()
+                                              awful.menu.client_list({ theme = { width = 250 } })
+                                          end),
+                     awful.button({ }, 4, function ()
+                                              awful.client.focus.byidx(1)
+                                          end),
+                     awful.button({ }, 5, function ()
+                                              awful.client.focus.byidx(-1)
+                                          end))
+
+-- Layouts
+-- ===================================================================
+-- Table of layouts to cover with awful.layout.inc, order matters.
+awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.floating,
+    awful.layout.suit.max,
+    -- awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile.bottom,
+    -- awful.layout.suit.tile.top,
+    -- awful.layout.suit.fair,
+    -- awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.spiral,
+    -- awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.corner.nw,
+    -- awful.layout.suit.magnifier,
+    -- awful.layout.suit.corner.ne,
+    -- awful.layout.suit.corner.sw,
+    -- awful.layout.suit.corner.se,
+}
+
+-- Wallpaper
+-- ===================================================================
+local function set_wallpaper(s)
+    -- Wallpaper
+    if beautiful.wallpaper then
+        local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        if type(wallpaper) == "function" then
+          wallpaper = wallpaper(s)
+        end
+
+        -- >> Method 1: Built in wallpaper function
+        gears.wallpaper.fit(wallpaper, s)
+        -- gears.wallpaper.maximized(wallpaper, s, true)
+
+        -- >> Method 2: Set theme's wallpaper with feh
+        --awful.spawn.with_shell("feh --bg-fill " .. wallpaper)
+
+        -- >> Method 3: Set last wallpaper with feh
+        -- awful.spawn.with_shell(os.getenv("HOME") .. "/.fehbg")
+    end
+end
+
+-- Set wallpaper
+awful.screen.connect_for_each_screen(function(s)
+    -- Wallpaper
+    set_wallpaper(s)
+end)
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", set_wallpaper)
+
+-- Tags
+-- ===================================================================
+awful.screen.connect_for_each_screen(function(s)
+    -- Each screen has its own tag table.
+    local l = awful.layout.suit -- Alias to save time :)
+    -- Tag layouts
+    local layouts = {
+        l.max,
+        l.max,
+        l.max,
+        l.max,
+        l.tile,
+        l.max,
+        l.max,
+        l.max,
+        l.tile,
+        l.max
+    }
+
+    -- Tag names
+    local tagnames = beautiful.tagnames or { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }
+    -- Create all tags at once (without seperate configuration for each tag)
+    awful.tag(tagnames, s, layouts)
+
+    -- Create tags with seperate configuration for each tag
+    -- awful.tag.add(tagnames[1], {
+    --     layout = layouts[1],
+    --     screen = s,
+    --     master_width_factor = 0.6,
+    --     selected = true,
+    -- })
+    -- ...
+
+    -- MOVE TO `elemental.bar` directory later
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(gears.table.join(
+                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist {
+        screen  = s,
+        filter  = awful.widget.taglist.filter.all,
+        buttons = taglist_buttons
+    }
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist {
+        screen  = s,
+        filter  = awful.widget.tasklist.filter.currenttags,
+        buttons = tasklist_buttons
+    }
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = "top", screen = s })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            mylauncher,
+            s.mytaglist,
+            s.mypromptbox,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            mykeyboardlayout,
+            wibox.widget.systray(),
+            mytextclock,
+            s.mylayoutbox,
+        },
+    }
+end)
+-- }}}
+
+
+-- Rules
+-- ===================================================================
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = {
+    -- All clients will match this rule.
+    {
+        rule = {},
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = keys.clientkeys,
+            buttons = keys.clientbuttons,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen
+        }
+    },
+    -- Floating clients.
+    {
+        rule_any = {
+            instance = {
+                "DTA", -- Firefox addon DownThemAll.
+                "copyq", -- Includes session name in class.
+                "pinentry"
+            },
+            class = {
+                "Arandr",
+                "Blueman-manager",
+                "Gpick",
+                "Kruler",
+                "MessageWin", -- kalarm.
+                "Sxiv",
+                "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+                "Wpa_gui",
+                "veromix",
+                "xtightvncviewer"
+            },
+            -- Note that the name property shown in xprop might be set slightly after creation of the client and the name shown there might not match defined rules here.
+            name = {
+                "Event Tester", -- xev.
+            },
+            role = {
+                "AlarmWindow", -- Thunderbird 's calendar.
+                "ConfigManager", -- Thunderbird 's about:config.
+                "pop-up", -- e.g.Google Chrome 's (detached) Developer Tools.
+            }
+        },
+        properties = {
+            floating = true
+        },
+    },
+    -- Add titlebars to normal clients and dialogs
+    {
+        rule_any = {
+            type = {
+                "normal",
+                "dialog"
+            }
+        },
+        properties = {
+            titlebars_enabled = true
+        },
+    },
+    --[[ -- Set Firefox to always map on the tag named "2" on screen 1.
+    {
+        rule = {
+            class = "Firefox"
+        },
+        -- properties = {
+            screen = 1,
+            tag = "2"
+        },
+    } ]]--
+}
+-- (Rules end here) ..................................................
+-- ===================================================================
+
+-- Signals
+-- ===================================================================
+-- Signal function to execute when a new client appears.
+client.connect_signal("manage", function (c)
+    -- Set the windows at the slave,
+    -- i.e. put it at the end of others instead of setting it master.
+    -- if not awesome.startup then awful.client.setslave(c) end
+
+    if awesome.startup
+      and not c.size_hints.user_position
+      and not c.size_hints.program_position then
+        -- Prevent clients from being unreachable after screen count changes.
+        awful.placement.no_offscreen(c)
+    end
+end)
+
+-- Add a titlebar if titlebars_enabled is set to true in the rules.
+client.connect_signal("request::titlebars", function(c)
+    -- buttons for the titlebar
+    local buttons = gears.table.join(
+        awful.button({ }, 1, function()
+            c:emit_signal("request::activate", "titlebar", {raise = true})
+            awful.mouse.client.move(c)
+        end),
+        awful.button({ }, 3, function()
+            c:emit_signal("request::activate", "titlebar", {raise = true})
+            awful.mouse.client.resize(c)
+        end)
+    )
+
+    awful.titlebar(c) : setup {
+        { -- Left
+            awful.titlebar.widget.iconwidget(c),
+            buttons = buttons,
+            layout  = wibox.layout.fixed.horizontal
+        },
+        { -- Middle
+            { -- Title
+                align  = "center",
+                widget = awful.titlebar.widget.titlewidget(c)
+            },
+            buttons = buttons,
+            layout  = wibox.layout.flex.horizontal
+        },
+        { -- Right
+            awful.titlebar.widget.floatingbutton (c),
+            awful.titlebar.widget.maximizedbutton(c),
+            awful.titlebar.widget.stickybutton   (c),
+            awful.titlebar.widget.ontopbutton    (c),
+            awful.titlebar.widget.closebutton    (c),
+            layout = wibox.layout.fixed.horizontal()
+        },
+        layout = wibox.layout.align.horizontal
+    }
+end)
+
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+    c:emit_signal("request::activate", "mouse_enter", {raise = false})
+end)
+
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+-- Garbage collection
+-- Enable for lower memory consumption
+-- ===================================================================
+-- collectgarbage("setpause", 160)
+-- collectgarbage("setstepmul", 400)
+collectgarbage("setpause", 110)
+collectgarbage("setstepmul", 1000)
